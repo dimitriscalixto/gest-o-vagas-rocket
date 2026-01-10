7b3882867f90c06972ccc8ea61b33d552973379a
dimitriscalixto <dimitrisc47@gmail.com>
2026-01-10 15:14:06 -0500

    Criando repositório

---
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,9 @@
+package br.com.rocket.gestao_vagas.modules.candidate;
+
+import br.com.rocket.gestao_vagas.modules.candidate.entity.CandidateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+}
--- src/main/java/br/com/rocket/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,20 +1,20 @@
 package br.com.rocket.gestao_vagas.modules.candidate.controllers;
 
+import br.com.rocket.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.rocket.gestao_vagas.modules.candidate.entity.CandidateEntity;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidate) {
-        System.out.println(candidate);
-        System.out.println(candidate.getUsername());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidate) {
+        return this.candidateRepository.save(candidate);
     }
 }
