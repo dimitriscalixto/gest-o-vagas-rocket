97f746692017dea2c6b7057b789de4df22b6e941
idimitrisc47 <dimitrisc47@gmail.com>
2026-01-10 11:02:59 -0500

    first commit

---
+++ src/test/java/br/com/rocket/gestao_vagas/GestaoDeVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocket.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
