bec1f34f73c12f9b24337b0f34bc9ffd38c0f899
idimitrisc47 <dimitrisc47@gmail.com>
2026-01-12 09:22:50 -0500

    Entidade Company

--- src/main/java/br/com/rocket/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,13 +1,14 @@
 package br.com.rocket.gestao_vagas.modules.candidate.controllers;
 
-import br.com.rocket.gestao_vagas.exceptions.UserFoundException;
-import br.com.rocket.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.rocket.gestao_vagas.modules.candidate.entity.CandidateEntity;
 import br.com.rocket.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
