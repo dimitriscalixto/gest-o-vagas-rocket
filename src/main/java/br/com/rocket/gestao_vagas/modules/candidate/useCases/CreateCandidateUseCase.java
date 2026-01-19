39292e11352fae0ff48d15edd2ce72cecaa6ffcc
idimitrisc47 <dimitrisc47@gmail.com>
2026-01-19 08:56:01 -0500

    Hash da senha do candidato

--- src/main/java/br/com/rocket/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.rocket.gestao_vagas.exceptions.UserFoundException;
 import br.com.rocket.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.rocket.gestao_vagas.modules.candidate.entity.CandidateEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,13 +14,17 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
-
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
         return this.candidateRepository.save(candidateEntity);
     }
 }
