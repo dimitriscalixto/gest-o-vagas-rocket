4f0420335cbee38cb4c62ddb643111f8b39e185d
idimitrisc47 <dimitrisc47@gmail.com>
2026-01-19 09:46:24 -0500

    Criando token de candidato

---
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDto.java
@@ -0,0 +1,4 @@
+package br.com.rocket.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDto (String username, String password) {
+}
---
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDto.java
@@ -0,0 +1,15 @@
+package br.com.rocket.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthCandidateResponseDto {
+
+    private String access_token;
+}
