4e0e70a9fbb786585d17f3b37e432346adf109ae
idimitrisc47 <dimitrisc47@gmail.com>
2026-01-20 09:15:57 -0500

    Controller: AuthCandidate

---
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -0,0 +1,29 @@
+package br.com.rocket.gestao_vagas.modules.candidate.controllers;
+
+import br.com.rocket.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDto;
+import br.com.rocket.gestao_vagas.modules.candidate.useCases.AuthCandidateUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCandidateController {
+
+    @Autowired
+    private AuthCandidateUseCase authCandidateUseCase;
+
+    @PostMapping("/candidate")
+    public ResponseEntity<Object> auth(@RequestBody AuthCandidateRequestDto authCandidateRequestDto) {
+        try {
+            var token = this.authCandidateUseCase.execute(authCandidateRequestDto);
+            return ResponseEntity.ok().body(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
