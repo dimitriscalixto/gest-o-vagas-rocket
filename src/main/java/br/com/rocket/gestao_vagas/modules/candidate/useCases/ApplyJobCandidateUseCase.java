a631c28a73e38f90993838542b03752f9ce448cc
idimitrisc47 <dimitrisc47@gmail.com>
2026-01-30 10:23:32 -0500

    UseCase: ApplyJobCandidate

---
+++ src/main/java/br/com/rocket/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -0,0 +1,33 @@
+package br.com.rocket.gestao_vagas.modules.candidate.useCases;
+
+import br.com.rocket.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.rocket.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.rocket.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.rocket.gestao_vagas.modules.job.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class ApplyJobCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    public void execute(UUID idCandidate, UUID idJob) {
+        this.candidateRepository.
+                findById(idCandidate)
+                .orElseThrow( () -> new UserNotFoundException());
+
+        this.jobRepository
+                .findById(idJob)
+                .orElseThrow( () -> new JobNotFoundException());
+
+
+
+    }
+}
